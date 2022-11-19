b56e7abf67f2480a3b7ff357c7cb70ea865a97f8
Andre Ostan <andre.ostan@gmail.com>
2022-11-19 09:01:05 -0300

    Swagger Docs Added

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -15,8 +20,11 @@ const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
 
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 // routes
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
